07e0b049b05565a9845681c655af6c9010047796
Andreas Wilke <andiwilke@gmail.com>
2021-10-11 13:37:07 +0200

    Updates to structure of the app

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+import urllib.parse
 import random
 import platform
 
@@ -20,23 +21,9 @@ def index():
     hostname = platform.node()
     return render_template('index.html', url=url, hostname=hostname, event_text=event_text, tweet_text=tweet_text, tweet_text_url=urllib.parse.quote(tweet_text))
 
-
-@app.route('/andreas')
-def andreas():
-    images = [
-        url_for('static', filename='beachops-1.png'),
-        url_for('static', filename='beachops-2.png'),
-        url_for('static', filename='norules-1.png'),
-        url_for('static', filename='norules-2.png'),
-    ]
-    url = random.choice(images)
-    hostname = platform.node()
-    return render_template('andreas.html', url=url, hostname=hostname)
-
-
 @app.route('/lacework')
 def lacework():
-    return render_template('lacework-scan.html')
+    return render_template('lacework.html')
 
 
 if __name__ == "__main__":
