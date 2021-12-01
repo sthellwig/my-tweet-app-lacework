8c4599177b5a7e94e5c34d9be20e4c9edc88d48a
Andreas Wilke <andiwilke@gmail.com>
2021-12-01 12:39:22 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import platform
 
 app = Flask(__name__)
 
-event_text = "Welcome to the Hops & DevOps of the 22nd of Sept in Berlin."
+event_text = "Welcome to the Hops & DevOps Christmas Edition"
 tweet_text = "Hello from Hops&DevOps. Just enjoyed a fantastic presentation from @automatecloud about Shift Left Security with #laceworks #hopsndevops"
 
 
