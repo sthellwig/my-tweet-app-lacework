79cb6e2dcc5af6ed36ea965f5ee07aa261ed5625
Andreas Wilke <andiwilke@gmail.com>
2022-02-23 11:55:20 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
-app_name = 'Demo Application for Dev and Hops'
+app_name = 'Demo Application for Dev and Hops event in Munich'
 
 print(app_name)
 
