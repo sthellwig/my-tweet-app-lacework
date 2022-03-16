4fb0940a76eb369a2dfb8f813d5e8a85a797ebc5
Andreas Wilke <andiwilke@gmail.com>
2022-03-16 18:13:36 +0100

    Now we are in Munich

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
-app_name = 'Demo Application for the Dev and Hops event in Cologne, March 2022'
+app_name = 'Demo Application for the Dev and Hops event in Munich, March 2022'
 
 print(app_name)
 
